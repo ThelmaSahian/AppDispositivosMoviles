e74fc2d3773a28632f13e8090465e8efefaf22d5
MaltosFits <48265021+MaltosFits@users.noreply.github.com>
2019-03-22 00:28:21 -0600

    Login Facebook
    configuracion del sdk de Facebook para el inicio de sesion con login button.

--- app/src/main/java/com/babyapp/Login.java
+++ app/src/main/java/com/babyapp/Login.java
@@ -1,5 +1,6 @@
 package com.babyapp;
 
+import android.content.Intent;
 import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,10 +8,22 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
 public class Login extends AppCompatActivity {
 
+    private LoginButton login_button;
+    private CallbackManager callbackManager;
+
+
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         final DataBaseHelper dataBaseHelper;
         super.onCreate(savedInstanceState);
@@ -42,5 +55,36 @@ public class Login extends AppCompatActivity {
 
             }
         });
+
+        callbackManager = CallbackManager.Factory.create();
+        login_button = (LoginButton) findViewById(R.id.login_button);
+        ((LoginButton) login_button).registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                goMainScreen();
+            }
+
+            @Override
+            public void onCancel() {
+                Toast.makeText(getApplicationContext(), R.string.com_facebook_loginview_cancel_action, Toast.LENGTH_SHORT).show();
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Toast.makeText(getApplicationContext(), R.string.com_facebook_smart_login_confirmation_cancel, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+    private void goMainScreen() {
+        Intent intent = new Intent(this, MainActivity.class);
+       // Intent.addFlags(intent.FLAG_ACTIVITY_CLEAR_TOP | intent.FLAG_ACTIVITY_CLEAR_TASK | intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 }
