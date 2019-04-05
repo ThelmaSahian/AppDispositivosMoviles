9ccaa799df4dd4df32ca94ca8964c313f2772a35
ThelmaSahian <45317904+ThelmaSahian@users.noreply.github.com>
2019-04-04 19:04:36 -0600

    login funcionando
    aún no guarda datos, pero reconoce al usuario y entra a Activity Usuarios

--- app/src/main/java/com/babyapp/Login.java
+++ app/src/main/java/com/babyapp/Login.java
@@ -3,7 +3,6 @@ package com.babyapp;
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Handler;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,7 +11,6 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
@@ -20,12 +18,6 @@ import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 import com.facebook.FacebookSdk;
 import com.facebook.appevents.AppEventsLogger;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthCredential;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FacebookAuthCredential;
-import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -88,9 +80,9 @@ public class Login extends AppCompatActivity {
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
-                handleFacebookAccessToken(loginResult.getAccessToken());
+                //handleFacebookAccessToken(loginResult.getAccessToken());
 
-                //goUsuariosScreen();
+                goUsuariosScreen();
             }
 
             @Override
@@ -104,7 +96,6 @@ public class Login extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(), R.string.com_facebook_smart_login_confirmation_cancel, Toast.LENGTH_SHORT).show();
             }
 
-
             public void onStart(){
                 Login.super.onStart();
                 FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
@@ -115,31 +106,15 @@ public class Login extends AppCompatActivity {
 
 
 
-    /*private void goUsuariosScreen() {
+    private void goUsuariosScreen() {
         Intent intent = new Intent(this, Usuarios.class);
         intent.addFlags(intent.FLAG_ACTIVITY_CLEAR_TOP | intent.FLAG_ACTIVITY_CLEAR_TASK | intent.FLAG_ACTIVITY_NEW_TASK);
         startActivity(intent);
-    }*/
+    }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data){
         super.onActivityResult(requestCode, resultCode, data);
         callbackManager.onActivityResult(requestCode, resultCode, data);
-    }
-
-    private void handleFacebookAccessToken(AccessToken token){
-        AuthCredential authCredential = FacebookAuthProvider.getCredential(token.getToken());
-        firebaseAuth.signInWithCredential(authCredential)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if(task.isSuccessful()){
-                            FirebaseUser user = firebaseAuth.getCurrentUser();
-
-                        }else{
-                            Toast.makeText(getApplicationContext(), "Authentication failed", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                });
     }
 }
